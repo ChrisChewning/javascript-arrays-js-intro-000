8fccf5b722bdc63319bd18e9a75b9433383b8367
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-18 01:38:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ];
+  
+  
+  function addElementToBeginningOfArray(array, element) {
+    var candyBars = [element, ...array]  
+    return candyBars;
+  }
+ 
+  function destructivelyAddElementToBeginningofArray(array, element) {
+    var candyBars = [element.unshift, array]
+    return candyBars;
+  }  
+    
